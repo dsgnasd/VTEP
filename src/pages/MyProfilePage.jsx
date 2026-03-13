f5328ac6b9570c28fd14a4a6e3c11bd5ff30d902
CLR <pavel.bendin@gmail.com>
2026-03-13 10:53:50 +0100

    Adjust spacing below profile tabs

--- src/pages/MyProfilePage.jsx
+++ src/pages/MyProfilePage.jsx
@@ -473,7 +473,7 @@ function ProfileTabsPanel({ activeTab, onTabChange, hasAvatar, onAvatarUpload })
           triggerClassName="px-3 py-1.5"
         />
       </div>
-      <div className="px-1.5 py-2.5 sm:px-2 sm:py-3">
+      <div className="px-1.5 pt-4 pb-2.5 sm:px-2 sm:pt-5 sm:pb-3">
         {activeTab === 'Обзор' && (
           <OverviewTab
             onNavigate={onTabChange}
